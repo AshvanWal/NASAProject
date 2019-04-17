fa78f3e65362b48ac95797e2ddfe45746274cd78
Ashvan <ashvan.wal@gmail.com>
2019-04-17 07:02:50 -0700

    Weather

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const hbs = require('hbs');
+const bodyParser = require('body-parser');
+const api_call1 = require('./public/api_call_1.js');
 const  port = process.env.PORT || 8080;
 
 var app = express();
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+
+app.use(bodyParser.json());
 app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
 
 hbs.registerPartials(__dirname + '/views/partials');
 hbs.registerHelper('getCurrentYear', () => {
@@ -11,10 +18,26 @@ hbs.registerHelper('getCurrentYear', () => {
 });
 
 app.get('/', (request, response) => {
-    response.render('home.hbs', {
+    response.render('form1.hbs', {
         title: 'Welcome Page',
         page1: ["/page1" , "/page2"]
     });
+});
+
+app.post('/weather', urlencodedParser, async (request, response) => {
+    // console.log(request.body.country_input);
+    try {
+        let weatherOut = await api_call1.get_weather(request.body.country_input);
+        // console.log(weatherOut);
+        response.render('page1.hbs', {
+            output: weatherOut.main
+        });
+    }catch (e) {
+        response.render('page1.hbs', {
+            output: e
+        });
+    }
+
 });
 
 
